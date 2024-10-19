f0962a45b5f25be834afc6c2246415da50d9038c
hafiihzafarhana <hafiihza2002@gmail.com>
2024-10-19 16:19:09 +0700

    give lazy load (suspense) on Front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,55 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-import LoginPage from "./pages/Login/Login";
-import RegisterPage from "./pages/Register/Register";
-import DashboardPage from "./pages/Dashboard/Dashboard";
-import NotFoundPage from "./pages/NotFound/NotFound";
 import { ModalProvider } from "./contexts/ModalContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import RedirectIfAuthenticated from "./components/RedirectIfAuthenticated";
+import { Suspense, lazy } from "react";
+
+const LoginPage = lazy(() => import("./pages/Login/Login"));
+const RegisterPage = lazy(() => import("./pages/Register/Register"));
+const DashboardPage = lazy(() => import("./pages/Dashboard/Dashboard"));
+const NotFoundPage = lazy(() => import("./pages/NotFound/NotFound"));
 
 function App() {
   return (
     <AuthProvider>
       <ModalProvider>
         <Router>
-          <Routes>
-            <Route
-              path="/login"
-              element={
-                <RedirectIfAuthenticated redirectTo="/">
-                  <LoginPage />
-                </RedirectIfAuthenticated>
-              }
-            />
-            <Route
-              path="/register"
-              element={
-                <RedirectIfAuthenticated redirectTo="/">
-                  <RegisterPage />
-                </RedirectIfAuthenticated>
-              }
-            />
-            <Route
-              path="/*"
-              element={
-                <ProtectedRoute redirectTo="/login">
-                  <DashboardPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="flex justify-center items-center h-screen">
+                <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+              </div>
+            }
+          >
+            <Routes>
+              <Route
+                path="/login"
+                element={
+                  <RedirectIfAuthenticated redirectTo="/">
+                    <LoginPage />
+                  </RedirectIfAuthenticated>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <RedirectIfAuthenticated redirectTo="/">
+                    <RegisterPage />
+                  </RedirectIfAuthenticated>
+                }
+              />
+              <Route
+                path="/*"
+                element={
+                  <ProtectedRoute redirectTo="/login">
+                    <DashboardPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Suspense>
         </Router>
       </ModalProvider>
     </AuthProvider>
--- frontend/src/pages/Dashboard/components/Right/Right.tsx
+++ frontend/src/pages/Dashboard/components/Right/Right.tsx
@@ -1,17 +1,29 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Explorer from "./components/Explorer";
-import NotFoundPage from "../../../NotFound/NotFound";
+import { Suspense, lazy } from "react";
+
+// Lazy load the components
+const Home = lazy(() => import("./components/Home"));
+const Explorer = lazy(() => import("./components/Explorer"));
+const NotFoundPage = lazy(() => import("../../../NotFound/NotFound"));
 
 const Right = () => {
   return (
     <div className="flex-1 p-[20px] bg-white">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explorer" element={<Explorer />} />
-        <Route path="/explorer/:id" element={<Explorer />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      {/* Suspense fallback is provided while components load */}
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explorer" element={<Explorer />} />
+          <Route path="/explorer/:id" element={<Explorer />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
